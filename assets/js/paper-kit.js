a1c08406ed9a6bf276843b7aeca889382d1926a5
Stefan Moldovan <moldovanste@gmail.com>
2017-05-22 18:45:14 +0300

    share buttons

--- assets/js/paper-kit.js
+++ assets/js/paper-kit.js
@@ -106,6 +106,111 @@ $(document).ready(function(){
         $(this).parent(".input-group").removeClass("input-group-focus");
     });
 
+    // Share buttons
+    if($('.twitter-sharrre').length != 0){
+        $('.twitter-sharrre').sharrre({
+          share: {
+            twitter: true
+          },
+          enableHover: false,
+          enableTracking: true,
+          enableCounter: false,
+          buttons: { twitter: {via: 'CreativeTim'}},
+          click: function(api, options){
+            api.simulateClick();
+            api.openPopup('twitter');
+          },
+          template: '<i class="fa fa-twitter"></i>',
+          url: 'http://demos.creative-tim.com/paper-kit/index.html'
+        });
+    }
+
+    if($('.twitter-sharrre-nav').length != 0){
+        $('.twitter-sharrre-nav').sharrre({
+          share: {
+            twitter: true
+          },
+          enableHover: false,
+          enableTracking: true,
+          enableCounter: false,
+          buttons: { twitter: {via: 'CreativeTim'}},
+          click: function(api, options){
+            api.simulateClick();
+            api.openPopup('twitter');
+          },
+          template: '<i class="fa fa-twitter"></i><p class="hidden-lg-up">Twitter</p>',
+          url: 'http://demos.creative-tim.com/paper-kit/index.html'
+        });
+    }
+
+    if($('.facebook-sharrre').length != 0){
+        $('.facebook-sharrre').sharrre({
+          share: {
+            facebook: true
+          },
+          enableHover: false,
+          enableTracking: true,
+          enableCounter: false,
+          click: function(api, options){
+            api.simulateClick();
+            api.openPopup('facebook');
+          },
+          template: '<i class="fa fa-facebook-square"></i>',
+          url: 'http://demos.creative-tim.com/paper-kit/index.html'
+        });
+    }
+
+    if($('.facebook-sharrre-nav').length != 0){
+        $('.facebook-sharrre-nav').sharrre({
+          share: {
+            facebook: true
+          },
+          enableHover: false,
+          enableTracking: true,
+          enableCounter: false,
+          click: function(api, options){
+            api.simulateClick();
+            api.openPopup('facebook');
+          },
+          template: '<i class="fa fa-facebook-square"></i><p class="hidden-lg-up">Facebook</p>',
+          url: 'http://demos.creative-tim.com/paper-kit/index.html'
+        });
+    }
+
+    if($('.linkedin-sharrre').length != 0){
+        $('.linkedin-sharrre').sharrre({
+          share: {
+            linkedin: true
+          },
+          enableCounter: false,
+          enableHover: false,
+          enableTracking: true,
+          click: function(api, options){
+            api.simulateClick();
+            api.openPopup('linkedin');
+          },
+          template: '<i class="fa fa-linkedin"></i>',
+          url: 'http://demos.creative-tim.com/paper-kit/index.html'
+        });
+    }
+
+    if($('.linkedin-sharrre-nav').length != 0){
+        $('.linkedin-sharrre-nav').sharrre({
+          share: {
+            linkedin: true
+          },
+          enableCounter: false,
+          enableHover: false,
+          enableTracking: true,
+          click: function(api, options){
+            api.simulateClick();
+            api.openPopup('linkedin');
+          },
+          template: '<i class="fa fa-linkedin"></i><p class="hidden-lg-up">LinkedIn</p>',
+          url: 'http://demos.creative-tim.com/paper-kit/index.html'
+        });
+    }
+
     demo.initPickColor();
 
      // Make the images from the card fill the hole space
